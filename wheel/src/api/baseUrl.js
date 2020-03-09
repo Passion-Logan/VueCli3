bf88ef1103df2b8263082402f3d02882ea328fec
Passion-Logan <wangdadamailbox@163.com>
2020-03-09 23:28:40 +0800

    添加修改

--- wheel/src/api/baseUrl.js
+++ wheel/src/api/baseUrl.js
@@ -1,18 +1,18 @@
 let baseUrl = ''
 console.log(process.env.NODE_ENV)
 switch (process.env.NODE_ENV) {
-  case 'dev':
+  case 'development':
     // 开发环境
     baseUrl = 'http://localhost:8081'
     break
   case 'test':
     // 测试环境
-    baseUrl = 'http://www.xxx.com/test'
+    baseUrl = 'http://localhost:8082'
     break
-  case 'prod':
+  case 'production':
     // 生产环境
-    baseUrl = 'http://www.xxx.com.cn/pro'
+    baseUrl = 'http://localhost:8083'
     break
 }
 
-export default baseUrl;
+export default baseUrl
